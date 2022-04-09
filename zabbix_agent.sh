10084ec04ffb1f523e846c47303b689a80866483
ipraptor <93579200+ipraptor@users.noreply.github.com>
2022-04-10 00:02:39 +0300

    Update zabbix_agent.sh

--- zabbix_agent.sh
+++ zabbix_agent.sh
@@ -12,11 +12,15 @@ then
   yum install -y zabbix-agent
   systemctl start zabbix-agent
   systemctl enable zabbix-agent
+  # Configuration zabbix agent after install (it works only at the first start, you need to write checks)
   sed -i 's/Server=127.0.0.1/Server=zabbix-server.local/' /etc/zabbix/zabbix_agentd.conf
   sed -i 's/ServerActive=127.0.0.1/ServerActive=zabbix-server.local/' /etc/zabbix/zabbix_agentd.conf
   sed -i 's/Hostname=Zabbix server/Hostname=zagent.local/' /etc/zabbix/zabbix_agentd.conf
+  # Firewall settings
   firewall-cmd --permanent --add-port=10050/tcp --add-port=10050/udp
   firewall-cmd --reload
+  # Selinux setting. ALERT! This config removes all zabbix agent actions from selinux control
+  semanage permissive -a zabbix_agent_t
 else
   echo "Please run this script by root"
 fi
