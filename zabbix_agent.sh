6c4f4e60045f59d79c38fde40e45a4966843faa2
ipraptor <93579200+ipraptor@users.noreply.github.com>
2022-04-09 23:35:41 +0300

    Update zabbix_agent.sh

--- zabbix_agent.sh
+++ zabbix_agent.sh
@@ -12,9 +12,9 @@ then
   yum install -y zabbix-agent
   systemctl start zabbix-agent
   systemctl enable zabbix-agent
-  sed -i 's/Server=/Server=zabbix-server.local/' /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/ServerActive=/ServerActive=zabbix-server.local/' /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/Hostname=/Hostname=zagent.local/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/Server=127.0.0.1/Server=zabbix-server.local/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/ServerActive=127.0.0.1/ServerActive=zabbix-server.local/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/Hostname=Zabbix server/Hostname=zagent.local/' /etc/zabbix/zabbix_agentd.conf
 else
   echo "Please run this script by root"
 fi
