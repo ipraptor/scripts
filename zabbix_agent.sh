1430f373eb494f4a8ac2d8abeba9d3e80d627ee4
ipraptor <93579200+ipraptor@users.noreply.github.com>
2022-04-10 00:46:31 +0300

    Update zabbix_agent.sh

--- zabbix_agent.sh
+++ zabbix_agent.sh
@@ -7,14 +7,15 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin/:/root/bin
 wh=$(whoami)
 if [[ $wh == "root" ]]
 then
+  read -p "Write IP\hostname your Zabbix server " -r zabip
   rpm -ivh http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
   dnf update -y
   yum install -y zabbix-agent
   systemctl start zabbix-agent
   systemctl enable zabbix-agent
   # Configuration zabbix agent after install (it works only at the first start, you need to write checks)
-  sed -i 's/Server=127.0.0.1/Server=zabbix-server.local/' /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/ServerActive=127.0.0.1/ServerActive=zabbix-server.local/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/Server=127.0.0.1/Server='zabip'/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/ServerActive=127.0.0.1/ServerActive='zabip'/' /etc/zabbix/zabbix_agentd.conf
   sed -i 's/Hostname=Zabbix server/Hostname=zagent.local/' /etc/zabbix/zabbix_agentd.conf
   # Firewall settings
   firewall-cmd --permanent --add-port=10050/tcp --add-port=10050/udp
