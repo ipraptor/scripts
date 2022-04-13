4a4ce0b1103945a9f141eb6102b81c792ac1bc87
ipraptor <93579200+ipraptor@users.noreply.github.com>
2022-04-13 10:53:27 +0300

    Update docker_portainer.sh

--- docker_portainer.sh
+++ docker_portainer.sh
@@ -8,6 +8,8 @@ wh=$(whoami)
 
 if [[ $wh = "root" ]]
 then
+	systemctl start docker
+	systemctl enable docker
 	docker volume create portainer_data
 	docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 	firewall-cmd --add-port=9000/tcp --permanent
