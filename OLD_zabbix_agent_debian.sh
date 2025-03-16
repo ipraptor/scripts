94046ee5f5c26b454e17a5fc3c2ea4f567cb7567
Anton Palamarchuk <93579200+ipraptor@users.noreply.github.com>
2025-03-16 23:00:48 +0300

    Update OLD_zabbix_agent_debian.sh

--- OLD_zabbix_agent_debian.sh
+++ OLD_zabbix_agent_debian.sh
@@ -7,7 +7,7 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin/:/root/bin
 wh=$(whoami)
 if [[ $wh == "root" ]]
 then
-  wget https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1+buster_all.deb
+  wget https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1+stretch_all.deb
   dpkg -i zabbix-release_4.4-1+buster_all.deb
   apt update
   apt install zabbix-agent
